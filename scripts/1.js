30594a9e1f682ee2b6da56e755518e0721b9815d
byshivam <byshivam1@gmail.com>
2023-02-19 15:51:54 +0530

    - Added Flash Effect - Full Black screen Not support notification - Improved UI

--- scripts/1.js
+++ scripts/1.js
@@ -4,17 +4,47 @@ console.log('%c Welcome to Byshivam Github \n\n%c- Don\'t Try anything Suspiciou
 
 window.addEventListener('load', ()=>{
     const notice = document.getElementById('news');
-    const card = document.getElementsByClassName('card')
-    const message = document.getElementById("message")
+    const content = document.getElementsByClassName('content');
+    const message = document.getElementById("message");
     notice.innerText = "This is a test message!";
 
     if (/Mobi|Android/i.test(navigator.userAgent)) {
         // If so, display the message in the center of the page
-        document.body.classList.add("hidden");
-        alert("This Site Only Support Desktop")
-      }
+        // document.body.classList.add("hidden");
+        // alert("This Site Only Support Desktop")
+        document.body.style.backgroundColor = 'black'
+        document.body.innerHTML = `<h3 style="text-align: center; padding: 25px; display: flex; justify-content: center; align-items: center; height: 100%;"> SORRY 🥲, THIS SITE IS DOESN'T NOT SUPPORT MOBILE DEVICES</h3>`
+        flash()
+    }
     else{
         message.style.display = 'none'
     }
 })
+
+
+function flash() {
+    var element = document.body;
+    var originalColor = element.style.backgroundColor;
+    var isRed = false;
+
+    var interval = setInterval(function() {
+      if (isRed) {
+        element.style.backgroundColor = originalColor;
+        element.style.color = 'red'
+        isRed = false;
+      } else {
+        element.style.textShadow = "0px 1px 12px red";
+        element.style.color = 'white'
+        isRed = true;
+      }
+    }, 200); // Flash every 200 milliseconds
+
+    setTimeout(function() {
+      clearInterval(interval); // Stop flashing after 5 seconds
+      element.style.backgroundColor = originalColor;
+      element.style.color = 'white';
+    }, 3000);
+  }
+
+//   flash();
 
