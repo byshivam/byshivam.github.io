2c01a75cd76c77ccb1dcbcacd6614773ee140111
byshivam <byshivam1@gmail.com>
2023-02-22 12:13:41 +0530

    + Added Mobile Support + Added Favicon + Fixed Bugs

--- scripts/1.js
+++ scripts/1.js
@@ -1,53 +1,23 @@
 console.log('%c Welcome to Byshivam Github \n\n%c- Don\'t Try anything Suspicious \n- Don\'t Try to Copy Site Code without Permission I can take Legal Action if I Find out \n\nThanks For Visiting! \n\n', 'background-color: black; color: #00FF33; font-size: 24px; padding: 20px 0;', 'color: #00FF33; font-size: 16px;');
 
-
+const desktopStylesheet = document.querySelector('link[href="css/style.css"]');
+const mobileStylesheet = document.querySelector('link[href="css/mob.css"]');
 
 window.addEventListener('load', ()=>{
     const loadingScreen = document.getElementById("loading-screen");
     loadingScreen.style.opacity = 0;
     setTimeout(() => loadingScreen.style.display = "none", 1000);
     const notice = document.getElementById('news');
-    const content = document.getElementsByClassName('content');
-    const message = document.getElementById("message");
+      // const message = document.getElementById("message");
     notice.innerText = "This is a test message!";
 
     if (/Mobi|Android/i.test(navigator.userAgent)) {
-        // If so, display the message in the center of the page
-        // document.body.classList.add("hidden");
-        // alert("This Site Only Support Desktop")
-        document.body.style.backgroundColor = 'black'
-        document.body.innerHTML = `<h3 style="text-align: center; padding: 25px; display: flex; justify-content: center; align-items: center; height: 100%;"> SORRY 🥲, THIS SITE IS DOESN'T NOT SUPPORT MOBILE DEVICES</h3>`
-        flash()
-    }
-    else{
-        message.style.display = 'none'
-    }
-})
-
-
-function flash() {
-    var element = document.body;
-    var originalColor = element.style.backgroundColor;
-    var isRed = false;
-
-    var interval = setInterval(function() {
-      if (isRed) {
-        element.style.backgroundColor = originalColor;
-        element.style.color = 'red'
-        isRed = false;
-      } else {
-        element.style.textShadow = "0px 1px 12px red";
-        element.style.color = 'white'
-        isRed = true;
+        desktopStylesheet.disabled = 'true';
+        mobileStylesheet[0].disabled = 'false';
       }
-    }, 200); // Flash every 200 milliseconds
-
-    setTimeout(function() {
-      clearInterval(interval); // Stop flashing after 5 seconds
-      element.style.backgroundColor = originalColor;
-      element.style.color = 'white';
-    }, 3000);
-  }
-
-//   flash();
+      else{
+        desktopStylesheet.disabled[0] = 'false'
+        mobileStylesheet.disabled = 'true'
+      }
+    })
 
